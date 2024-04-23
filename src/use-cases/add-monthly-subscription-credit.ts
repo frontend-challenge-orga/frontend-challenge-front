20c0730f7674abe8e89c75325ec29e77378d2998
Swiichy <adlpromail@gmail.com>
2024-04-23 07:44:03 +0200

    refact: addMonthlySubscriptionCredit

--- src/use-cases/add-monthly-subscription-credit.ts
+++ src/use-cases/add-monthly-subscription-credit.ts
@@ -3,9 +3,7 @@ import { getCredit, createCredit, updateCredit } from "@/data-access/credit";
 export async function addMonthlySubscriptionCredit(userId: string) {
   const userCredits = await getCredit(userId);
 
-  if (userCredits) {
-    await updateCredit(userId);
-  } else {
-    await createCredit(userId);
-  }
+  userCredits ? await updateCredit(userId) : await createCredit(userId);
+
+  // Send email to user
 }
