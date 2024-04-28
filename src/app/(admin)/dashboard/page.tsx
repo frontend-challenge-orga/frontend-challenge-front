848cf810360c49ea71bf706c019b545da91a1d84
Willem Jouret <willem.jouret9@gmail.com>
2024-04-28 20:40:32 +0200

    first commit

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { Fragment } from "react";
 import { AnalyticsNavigation } from "@/core/views/modules/admin/layouts/navigation/analytics-navigation";
 
 export default async function AdminPage() {
+  // Hello World !
   return (
     <Fragment>
       <AnalyticsNavigation />
